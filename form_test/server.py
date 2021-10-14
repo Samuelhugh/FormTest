ea59f2238347ebd98d49018a18affa29a992a96a
samuel hughes <samuelhugh33@gmail.com>
2021-10-14 04:20:04 -0500

    Learning how to render form data

---
+++ form_test/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "keep it secret, keep it safe" # Set a Secret Key for Security Purposes. Also it should be a long random byte or str.
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/users', methods=['POST']) # METHODS not METHOD, Also Methods can have a number of other values (i.e "GET") because the values are store in a List.
+def create_user():
+    print('Got Post Info')
+    print(request.form)
+    # Adding the Two Properties to Session to store the Name and Email.
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    # Never Render a Template on a POST request.
+    # Instead I will Redirect to my Index Route.
+    return redirect('/show') # Redirecting.
+
+
+# adding this method
+@app.route("/show")
+def show_user():
+    print("Showing the User Info From the Form")
+    print(request.form)
+    return render_template("show.html", name_on_template=session['username'], email_on_template=session['useremail'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
